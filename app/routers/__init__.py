b6f7ea3a1c0b85876b27b89ffab79221373ea290
PavelDaneliya <95652603+DaneliyaPavel@users.noreply.github.com>
2025-06-29 22:38:38 +0300

    Добавить уведомления через Telegram

---
+++ app/routers/__init__.py
@@ -0,0 +1,13 @@
+"""Регистрация модулей маршрутов."""
+
+from . import categories, transactions, goals, users, analytics, tinkoff, banks
+
+__all__ = [
+    "categories",
+    "transactions",
+    "goals",
+    "users",
+    "analytics",
+    "tinkoff",
+    "banks",
+]
