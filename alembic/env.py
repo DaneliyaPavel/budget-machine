74a9145c9e6d022fd87ae38556c16ea6f0bfcd3a
PavelDaneliya <95652603+DaneliyaPavel@users.noreply.github.com>
2025-07-03 00:54:46 +0300

    feat: add comprehensive uuid schema migration

--- alembic/env.py
+++ alembic/env.py
@@ -4,7 +4,7 @@ import os
 from alembic import context  # type: ignore[attr-defined]
 from sqlalchemy import engine_from_config, pool
 from sqlalchemy.engine import make_url
-from backend.app.models import Base
+from backend.app.database import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
